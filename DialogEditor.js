d8b6df3d95642d8d5b80220f98104c69b8e936a5
brwhale <lazorpwnage@gmail.com>
2022-08-06 10:05:58 -0700

    setting up the project

---
+++ DialogEditor.js
@@ -0,0 +1,21 @@
+let nodes = [];
+
+function addNode() {}
+
+function connectResponse(parent, child) { }
+
+function setMessage(index, message) { }
+
+function setVisibilityScript(index, script) { }
+
+function setActivationScript(index, script) { }
+
+function saveToFile() { }
+
+function loadFromFile() { }
+
+function drawNodes() {
+    document.querySelector("p").innerHTML = "changed";
+}
+
+drawNodes();
