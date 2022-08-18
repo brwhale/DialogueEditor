3ec8897a32ef27d4a725c518c747271f480a80a7
brwhale <lazorpwnage@gmail.com>
2022-08-18 02:38:32 -0700

    add resizing and sorting

--- DialogEditor.js
+++ DialogEditor.js
@@ -1,9 +1,10 @@
 let nodes = [];
 let addingChildrenNode = -1;
 let draggingNode = -1;
+let resizeMode = false;
 
 function addNode() {
-    nodes.push({id: nodes.length, message: "", hideScript: "", actScript: "", soundFile: "", pos: {x: 100, y: document.querySelector("#nodesBox").clientHeight }, children: []});
+    nodes.push({id: nodes.length, message: "", hideScript: "", actScript: "", soundFile: "", width: 400, pos: {x: 100+nodes.length, y: document.querySelector("#nodesBox").clientHeight }, children: []});
 
     addingChildrenNode = -1;
     drawNodes();
@@ -15,13 +16,14 @@ function deleteNode(index) {
         if (nodes[i].id > index) {
             nodes[i].id -= 1;
         }
-        nodes[i].children.filter(function(item){ return item !== index});
+        nodes[i].children = nodes[i].children.filter(function(item){ return item != index; });
         nodes[i].children.forEach(function(part, idx) {
             if (nodes[i].children[idx] > index) {
                 nodes[i].children[idx] -= 1;
             }
           });
     }
+    drawNodes();
 }
 
 function connectResponse(parent, child) {
@@ -68,33 +70,47 @@ function getArrow(start, end) {
     let height = Math.max(start.y, end.y) - miny + padding*2;
     let minx = Math.min(start.x, end.x);
     let width = Math.max(start.x, end.x) - minx + padding*2;
+    let color = "red";
     let content = '<svg width="'+width+'" height="'+height+'" style="pointer-events: none;position: absolute;top: ' + (miny-padding) + 'px;left: '+(minx-padding)+'px;">';
     content += '<defs><marker id="arrowtip" markerWidth="13" markerHeight="13" refx="2" refy="6" orient="auto">';
-    content += '<path d="M2,2 L2,11 L10,6 L2,2" style="fill:red;" /></marker></defs>';
+    content += '<path d="M2,2 L2,11 L10,6 L2,2" style="fill:'+color+';" /></marker></defs>';
     content += '<path d="M' + (start.x-minx+padding) + "," + (start.y-miny+padding) + ' L' + (end.x-minx+padding) + "," + (end.y-miny+padding); 
-    content += '"style="stroke:red; stroke-width: 1.25px; fill: none; marker-end: url(#arrowtip);"/></svg>';
+    content += '"style="stroke:'+color+'; stroke-width: 1.25px; fill: none; marker-end: url(#arrowtip);"/></svg>';
     return content;
 }
 
-function updateMessage(index) {
-    nodes[index].message = document.getElementById("message"+index).value;
+function updateMessage(index, elem) {
+    textAreaAdjust(elem);
+    nodes[index].message = elem.value;
+    drawArrows();
 }
 
-function updateVisScript(index) {
-    nodes[index].hideScript = document.getElementById("hideScript"+index).value;
+function updateVisScript(index, elem) {
+    textAreaAdjust(elem);
+    nodes[index].hideScript = elem.value;
+    drawArrows();
 }
 
-function updateActScript(index) {
-    nodes[index].actScript = document.getElementById("actScript"+index).value;
+function updateActScript(index, elem) {
+    textAreaAdjust(elem);
+    nodes[index].actScript = elem.value;
+    drawArrows();
 }
 
-function updateFile(index) {
-    nodes[index].soundFile = document.getElementById("soundFile"+index).value;
+function updateFile(index, elem) {
+    nodes[index].soundFile = elem.value;
 }
 
 function mousedownNode(index) {
     event.stopPropagation();
     draggingNode = index;
+    resizeMode = false;
+}
+
+function resizeNode(index) {
+    event.stopPropagation();
+    draggingNode = index;
+    resizeMode = true;
 }
 
 function mouseup() {
@@ -107,8 +123,48 @@ function clearSelections() {
     document.getElementById("arrow").innerHTML = "";
 }
 
+function textAreaAdjust(element) {
+    element.style.height = "1px";
+    element.style.height = (25+element.scrollHeight)+"px";
+  }
+
+function sortNodes() {
+    let alreadyMoved = [];
+    let usedPositions = [];
+    let y = 40;
+    let h = 500;
+    nodes.forEach((item, index, array) => {
+        item.children.forEach((c, i, arr) => {
+            if (!c) return;
+            if (!alreadyMoved.includes(c)) {
+                alreadyMoved.push(c);
+                let pos = {x:i * 500 + item.pos.x, y: h + item.pos.y};
+                while (usedPositions.some(e => e.x == pos.x && e.y == pos.y)) {
+                    pos.x += 500;
+                }
+                usedPositions.push(pos);
+                nodes[c].pos.x = pos.x;
+                nodes[c].pos.y = pos.y;
+            }
+        });
+      });
+    drawNodes();
+}
+
+function drawArrows() {
+    document.querySelector("#arrow").innerHTML = ''; // clear temp arrow
+    let selector = document.querySelector("#arrows");
+    selector.innerHTML = '';
+    nodes.forEach((item, index, array) => {
+        let elem = document.getElementById("node"+index);
+        let origin = {x:item.pos.x + elem.clientWidth - 20, y:item.pos.y + elem.clientHeight};
+        item.children.forEach((c, i, arr) => {
+            selector.innerHTML += getArrow(origin, {x:nodes[c].pos.x + 200, y:nodes[c].pos.y + 50});
+        });
+      });
+}
+
 function drawNodes() {
-    document.getElementById("arrow").innerHTML = '';
     let selector = document.querySelector("#nodesBox");
     let biggest = {x:0, y:0};
     selector.innerHTML = "";
@@ -121,34 +177,36 @@ function drawNodes() {
         }
         let content = "<div id='node"+index+"' class='node absolute' style='top:" + item.pos.y + "px;left:" + item.pos.x + "px;' onclick='clickedNode(" + index + ")'>";
         content += "<p style='display:inline-block;'>Id: " + index + " </p>";
-        content += "<a style='display:inline-block;'class='button adder' onclick='wantsChild(" + index + ")'>Add Child</a>";
-        content += "<a style='display:inline-block;'class='button mover' onmousedown='mousedownNode(" + index + ")'>Move</a>";
-        content += "<textarea id='message"+index+"'class='message' placeholder='Message' onkeyup='updateMessage("+index+")'>";
+        content += "<a style='display:inline-block;float:right;'class='button stretcher' onmousedown='resizeNode(" + index + ")'>Resize</a>";
+        content += "<a style='display:inline-block;float:right;'class='button mover' onmousedown='mousedownNode(" + index + ")'>Move</a>";
+        content += "<textarea id='message"+index+"'class='message' style='width: "+item.width+"px;' placeholder='Message' onkeyup='updateMessage("+index+", this)'>";
         content += item.message;
         content += "</textarea>";
-        content += "<textarea id='hideScript"+index+"' class='message' placeholder='Visibility Script' onkeyup='updateVisScript("+index+")'>";
+        content += "<textarea id='hideScript"+index+"' class='message' style='width: "+item.width+"px;' placeholder='Visibility Script' onkeyup='updateVisScript("+index+", this)'>";
         content += item.hideScript;
         content += "</textarea>";
-        content += "<textarea id='actScript"+index+"' class='message' placeholder='Activation Script' onkeyup='updateActScript("+index+")'>";
+        content += "<textarea id='actScript"+index+"' class='message' style='width: "+item.width+"px;' placeholder='Activation Script' onkeyup='updateActScript("+index+", this)'>";
         content += item.actScript;
         content += "</textarea>";
-        content += "<input id='soundFile"+index+"' placeholder='Sound File' onkeyup='updateFile("+index+")'>";
+        content += "<input id='soundFile"+index+"' class='message' placeholder='Sound File' onkeyup='updateFile("+index+", this)' value='";
         content += item.soundFile;
-        content += "</input>";
+        content += "'/>";
+        content += "<a style='display:inline-block;'class='button deleter' onclick='deleteNode(" + index + ")'>Delete</a>";
+        content += "<a style='display:inline-block;float: right;'class='button adder' onclick='wantsChild(" + index + ")'>Add Child</a>";
         content += "</div>";
         selector.innerHTML += content;
       });
 
-      nodes.forEach((item, index, array) => {
-        item.children.forEach((c, i, arr) => {
-            selector.innerHTML += getArrow({x:item.pos.x + 100, y:item.pos.y + 50}, {x:nodes[c].pos.x + 200, y:nodes[c].pos.y + 50});
-        });
+      document.querySelectorAll(".message").forEach((item) => {
+        textAreaAdjust(item);
       });
 
       if (biggest.x > 0) {
         selector.style.width = 500+biggest.x+"px";
-        selector.style.height = 300+biggest.y+"px";
+        selector.style.height = 400+biggest.y+"px";
       }
+
+      drawArrows();
 }
 
 function updateAddArrow(origin, point) {
@@ -158,10 +216,15 @@ function updateAddArrow(origin, point) {
 addEventListener('mousemove', (event) => {
     if (addingChildrenNode >= 0) {
         let origin = nodes[addingChildrenNode].pos;
-        updateAddArrow({x:origin.x + 100, y:origin.y+50}, {x:event.clientX + window.pageXOffset, y:event.clientY + window.pageYOffset });
+        let elem = document.getElementById("node"+addingChildrenNode);
+        updateAddArrow({x:origin.x + elem.clientWidth - 20, y:origin.y+elem.clientHeight}, {x:event.clientX + window.pageXOffset, y:event.clientY + window.pageYOffset });
     } else if (draggingNode >= 0) {
-        nodes[draggingNode].pos.x += event.movementX;
-        nodes[draggingNode].pos.y += event.movementY;
+        if (resizeMode) {
+            nodes[draggingNode].width += event.movementX;
+        } else {
+            nodes[draggingNode].pos.x += event.movementX;
+            nodes[draggingNode].pos.y += event.movementY;
+        }
         drawNodes();
     }
 });
@@ -193,6 +256,17 @@ function exportJson() {
     download(getJson());
 }
 
+function repairNodeMetadata() {
+    nodes.forEach((item, index, array) => {
+        if (!item.pos) {
+            item.pos = {x:100+index, y:index*350};
+        }
+        if (!item.width) {
+            item.width = 400;
+        }
+      });
+}
+
 function importFile(e) {
     var file = e.target.files[0];
     if (!file) {
@@ -201,6 +275,7 @@ function importFile(e) {
     var reader = new FileReader();
     reader.onload = function(e) {
         nodes = JSON.parse(e.target.result);
+        repairNodeMetadata();
         drawNodes();
     };
     reader.readAsText(file);
